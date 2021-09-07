e39aaaca3ce8a22e672000f18e5dbb6557572dd4
Pradeep Sharma <Pradeep.Sharma@BDINDIA.LOCAL>
2021-09-07 15:42:57 +0530

    initial commit

---
+++ src/main/java/org/bootcamp/stuff/instructor/InstructorApiApplication.java
@@ -0,0 +1,13 @@
+package org.bootcamp.stuff.instructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bootcamp/stuff/instructor/InstructorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bootcamp.stuff.instructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
